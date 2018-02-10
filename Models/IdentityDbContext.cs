008c41551ad2a5c7da89f9b9f7a33fef3fd40e78
orzech <michalorzechowski123@gmail.com>
2018-02-10 20:37:49 +0100

    identity

---
+++ Models/IdentityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Atut.Models
+{
+    public class IdentityDbContext : IdentityDbContext<User>
+    {
+        public IdentityDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -3,32 +3,67 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Atut.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
 
 namespace Atut
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+            Env = env;
         }
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        private IHostingEnvironment Env { get; set; }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //            RegisterServices(services);
+
+            services.AddDbContext<IdentityDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            IMvcBuilder mvc = services.AddMvc(options =>
+            {
+//                options.Filters.Add(typeof(ApiExceptionFilter));
+            });
+
+//            IndentJsonForDevelopment(mvc);
+
+//            services.Configure<ConfigurationManager>(Configuration.GetSection("AppSettings"));
+//            services.Resolve<IJWtHelper>().AddJwtAuthentication(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory
+//            IDatabaseManager databaseManager
+            )
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -37,6 +72,8 @@ namespace Atut
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            loggerFactory.AddFile("Logs/logs-{Date}.txt", LogLevel.Error);
 
             app.UseStaticFiles();
             
@@ -54,5 +91,23 @@ namespace Atut
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+//        private void RegisterServices(IServiceCollection services)
+//        {
+//            services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CallGateConnectionString")));
+//            services.AddAutoMapper();
+//            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+//            DependencyInjectionRegisterer.RegisterAssemblies(services);
+//
+//            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+//            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+//            services.AddScoped<IUrlHelper>(provider =>
+//            {
+//                var actionContext = provider.GetService<IActionContextAccessor>().ActionContext;
+//                return new UrlHelper(actionContext);
+//            });
+//            services.AddTransient<ISeeder, UserSeeder>();
+//            services.AddTransient<ISeeder, GroupSeeder>();
+//        }
     }
 }

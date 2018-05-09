65c2af40e77f98fea2580d966044138b2df0f872
orzech <michalorzechowski123@gmail.com>
2018-05-09 20:24:01 +0200

    ***

--- Scripts/journey/JourneyIndexViewModel.js
+++ Scripts/journey/JourneyIndexViewModel.js
@@ -1,6 +1,7 @@
 ﻿import Vue from "vue";
 import { ClientTable } from "vue-tables-2";
 import moment from 'moment';
+import Datepicker from 'vuejs-datepicker';
 
 moment.locale('pl');
 window.moment = moment;
@@ -9,13 +10,16 @@ Vue.use(ClientTable);
 
 var journeyIndexViewModel = function (model) {
     var columns = ["vehiclesColumn", "startingPlace", "finalPlace", "startDate", "endDate", "actions"];
-
+    
     if (!!model.isAdmin) {
         columns.splice(0, 0, "companyNameShort");
     }
 
     var vue = new Vue({
         el: "#JourneyIndex",
+        components: {
+            Datepicker
+        },
         data: {
             columns: columns,
             options: {
@@ -34,7 +38,11 @@ var journeyIndexViewModel = function (model) {
                 sortable: ["companyNameShort", "startingPlace", "finalPlace", "startDate", "endDate"],
                 perPage: 20
             },
-            data: model
+            data: model,
+            filterCompany: null,
+            filterFromDate: null,
+            filterToDate: null,
+            filterCountry: null,
         }
     });
 }
